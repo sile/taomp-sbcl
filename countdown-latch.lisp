2d7f3ef9d2bda3ed885a5171014062b3e4437ce3
Takeru Ohta <phjgt308@gmail.com>
2011-07-30 03:31:11 +0900

    atomic-defの代わりにcompare-and-swapを使うように変更

--- countdown-latch.lisp
+++ countdown-latch.lisp
@@ -4,21 +4,22 @@
            countdown-and-await))
 (in-package :countdown-latch)
 
-(deftype atomic-int ()
-  #+x86-64 '(unsigned-byte 64)
-  #+x86 '(unsigned-byte 32))
-
 (defstruct latch 
-  (count 0 :type atomic-int))
+  (count 0 :type fixnum))
 
 (defun make (count)
-  (make-latch :count count))
+  (make-latch :count (max 0 count)))
 
 (defmacro barrier (exp)
   `(sb-thread:barrier (:data-dependency) ,exp))
 
+(defun atomic-decriment-if-plus (latch)
+  (let* ((old (latch-count latch))
+         (new (1- old)))
+    (when (plusp old)
+      (unless (eq old (sb-ext:compare-and-swap (latch-count latch) old new))
+        (atomic-decriment-if-plus latch)))))
+
 (defun countdown-and-await (latch)
-  (let ((old (latch-count latch)))
-    (sb-ext:atomic-decf (latch-count latch))
-    (loop FOR cur = (barrier (latch-count latch))
-          WHILE (and (< 0 cur old)))))
+  (atomic-decriment-if-plus latch)
+  (loop UNTIL (barrier (zerop (latch-count latch)))))
