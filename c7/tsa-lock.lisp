65f6ee5cea731d7a960cd5294fde86249f5bb0c2
ohta <phjgt308@gmail.com>
2011-09-16 19:13:10 +0900

    TSA-LOCK追加

---
+++ c7/tsa-lock.lisp
@@ -0,0 +1,45 @@
+(defpackage tsa-lock
+  (:use :common-lisp :util)
+  (:export make
+           lock
+           unlock
+           with-lock))
+(in-package :tsa-lock)
+
+(defstruct lock-obj
+  (flag nil :type boolean))
+
+(defmacro test-and-set-true (flag)
+  `(sb-ext:compare-and-swap ,flag nil t))
+  
+
+(defun make ()
+  (make-lock-obj))
+
+(defun lock (lock-obj)
+  (loop WHILE (test-and-set-true (lock-obj-flag lock-obj))))
+
+(defun unlock (lock-obj)
+  (setf (lock-obj-flag lock-obj) nil))
+
+(define-with-lock-macro with-lock lock unlock)
+
+#|
+(defun tsa-lock-test (lock-obj latch &aux (tid (thread-id:get)))
+  (countdown-latch:countdown-and-await latch)
+  
+  (loop FOR i FROM 0 TO 10
+    DO
+    (tsa-lock:with-lock lock-obj
+      (format t "~&; <THREAD ~a> ~a~%" tid i)))
+  'done)
+
+(let* ((thread-num 5)
+       (latch (countdown-latch:make thread-num))
+       (lock (tsa-lock:make)))
+  (loop REPEAT (1- thread-num)
+        DO (sb-thread:make-thread 
+            (lambda () 
+              (tsa-lock-test lock latch))))
+  (tsa-lock-test lock latch))
+|#
--- taomp.asd
+++ taomp.asd
@@ -9,9 +9,15 @@
                (:file "countdown-latch")
                (:file "tls")
                (:file "thread-id")
+
+               ;; chapter 2
                (:file "c2/lock-one")
                (:file "c2/lock-two")
                (:file "c2/peterson-lock")
                (:file "c2/filter-lock")
                (:file "c2/bakery-lock")
+
+               ;; chapter 7
+               (:file "c7/tsa-lock")
+               
                ))
